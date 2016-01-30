09f5980d5eadf32c2d184c69b5d20676c0bd1569
SPJ2EE <sp.job@ukr.net>
2016-01-30 16:22:50 +0200

    maven

---
+++ src/test/java/sp/lesson/CalculatorTest.java
@@ -0,0 +1,14 @@
+package sp.lesson;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Aramat on 30.01.2016.
+ */
+public class CalculatorTest {
+
+    @org.junit.Test
+    public void testAdd() throws Exception {
+
+    }
+}
